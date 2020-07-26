ece0c8c3ad071d2b786df8242abf6b5863882688
jlhung <35291112+jlhung@users.noreply.github.com>
2020-07-26 15:49:29 +0800

    a004

---
+++ a004- 文文的求婚.py
@@ -0,0 +1,15 @@
+'''
+20200717    v1.0    jlhung  
+'''
+
+while True:
+    try:
+        n = int(input())
+        
+        if (n % 4 == 0 and n % 100 != 0) or n % 400 == 0:
+            print("閏年")
+        else:
+            print("平年")
+            
+    except(EOFError):
+        break
